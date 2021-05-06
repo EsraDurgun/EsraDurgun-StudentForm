2da2ecf56fb660dfd516402977409bf38b90a78a
hp <esradurgun100083@hotmail.com>
2021-05-06 12:48:18 +0300

    Proje dosyası ekle.

---
+++ EsraDurgun-StudentForm/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EsraDurgun_StudentForm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ EsraDurgun-StudentForm/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EsraDurgun_StudentForm.Models
+{
+    public class Student
+    {
+        public Student()
+        {
+            CreationDate = DateTime.Now;
+        }
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+        
+        public string Department { get; set; }
+
+        [Required]
+        public DateTime BirtDate { get; set; }
+
+        
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime LastUpDate { get; set; }
+    }
+}
